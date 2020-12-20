471feab1860d97f1a879b647bc2b2dbe871c4f13
akthecoders <akshay@akthecoder.com>
2020-12-20 16:41:36 +0530

    Update Chess v1

--- Chess/Chess_v_1.java
+++ Chess/Chess_v_1.java
@@ -9,103 +9,62 @@ package Chess;
  * <p>
  * <p>
  * <p>
- * GET: {baseURL}/chess/initiate
- * Responses:
- * 200:
- * Return a json object for the status if chess is initiated with a game id.
- * body: Contains json
- * 404:
- * Return if chess not initialized
+ * GET: {baseURL}/chess/initiate Responses: 200: Return a json object for the
+ * status if chess is initiated with a game id. body: Contains json 404: Return
+ * if chess not initialized
  * <p>
  * <p>
  * <p>
  * <p>
  * <p>
- * GET: {baseURL}/chess/{gameId}/player/{playerId}/move?from={location}?to={location}
- * Path Params:
- * playerId: playerId of the player who is doing the move.
- * values: A valid playerId
- * type: String
- * gameId: gameId is the id of the game which is beign played.
- * values: A valid gameId
- * type: String
- * Query Params:
- * from: location from where the player wants to move
- * values: A string of the numeric and the alphabets as per the chess board.
- * type: String
- * to: locatino to where the player wants to move
- * values: A string of the numeric and the alphabets as per the chess board.
- * type: String
- * Response:
- * 200:
- * Return a json object if the move was valid, along with an object containig values if it attacked the another player pieces.
- * body: Contains json of valid move along with , piece if any attacked by this move.
- * 401:
- * Return a json object if the move was not valid,
- * body: Contains json of invalid move.
+ * GET:
+ * {baseURL}/chess/{gameId}/player/{playerId}/move?from={location}?to={location}
+ * Path Params: playerId: playerId of the player who is doing the move. values:
+ * A valid playerId type: String gameId: gameId is the id of the game which is
+ * beign played. values: A valid gameId type: String Query Params: from:
+ * location from where the player wants to move values: A string of the numeric
+ * and the alphabets as per the chess board. type: String to: locatino to where
+ * the player wants to move values: A string of the numeric and the alphabets as
+ * per the chess board. type: String Response: 200: Return a json object if the
+ * move was valid, along with an object containig values if it attacked the
+ * another player pieces. body: Contains json of valid move along with , piece
+ * if any attacked by this move. 401: Return a json object if the move was not
+ * valid, body: Contains json of invalid move.
  * <p>
  * <p>
  * <p>
  * <p>
  * <p>
- * GET: {baseURL}/chess/{gameId}/player/{playerId}/winner
- * Path Params:
- * gameId: gameId is the id of the game which is beign played.
- * values: A valid gameId
- * type: String
- * playerId: playerId of the player who is doing the move.
- * values: A valid playerId
- * type: String
- * Response:
- * 200:
- * Return a json object with status of game and if the player has won or defeated or no status
- * body: Contains json
- * 500:
- * Return a json object if request fails due to server error.
+ * GET: {baseURL}/chess/{gameId}/player/{playerId}/winner Path Params: gameId:
+ * gameId is the id of the game which is beign played. values: A valid gameId
+ * type: String playerId: playerId of the player who is doing the move. values:
+ * A valid playerId type: String Response: 200: Return a json object with status
+ * of game and if the player has won or defeated or no status body: Contains
+ * json 500: Return a json object if request fails due to server error.
  * <p>
  * <p>
  * <p>
  * <p>
  * <p>
- * Database Design for Chess
- * Tables:
- * Game
- * User
- * GameUserWonMapping
- * Moves
+ * Database Design for Chess Tables: Game User GameUserWonMapping Moves
  * <p>
- * Game Table
- * ColName             Data Type               Constraints
- * id                  var_char(10)            PK
- * status              ENUM(5)                 Not Null
- * player1             var_char(10)            Foreign Key to User
- * player2             var_char(10)            Foreign Key to User
+ * Game Table ColName Data Type Constraints id var_char(10) PK status ENUM(5)
+ * Not Null player1 var_char(10) Foreign Key to User player2 var_char(10)
+ * Foreign Key to User
  * <p>
  * <p>
- * User Table
- * ColName             Data Type               Constratints
- * id                  var_char(10)            PK
- * first_name          var_char(20)            Not Null
- * last_name           var_char(20)            Not NUll
- * email               var_char(30)            Not Null CK
+ * User Table ColName Data Type Constratints id var_char(10) PK first_name
+ * var_char(20) Not Null last_name var_char(20) Not NUll email var_char(30) Not
+ * Null CK
  * <p>
- * GameUserWonMapping
- * ColName             Data Type               Constraints
- * id                  var_char(10)            PK
- * gameID              var_char(10)            ForeignKey to Game Table
- * userId              var_char(10)            ForeignKey to User
+ * GameUserWonMapping ColName Data Type Constraints id var_char(10) PK gameID
+ * var_char(10) ForeignKey to Game Table userId var_char(10) ForeignKey to User
  * <p>
- * Moves
- * ColName             Data Type               Constraints
- * id                  var_char(10)            PK
- * Piece               var_char(10)            Not Null
- * LocationFrom        var_char(10)            Not NUll
- * LocationTo          var_char(10)            Not Null
- * LastMove            boolean                 Not Null
- * PrevMove            var_char(10)            Key to Moves.ID
- * NextMove            var_char(10)            Key to Moves.ID
+ * Moves ColName Data Type Constraints id var_char(10) PK Piece var_char(10) Not
+ * Null LocationFrom var_char(10) Not NUll LocationTo var_char(10) Not Null
+ * LastMove boolean Not Null PrevMove var_char(10) Key to Moves.ID NextMove
+ * var_char(10) Key to Moves.ID
  */
-
 
 public class Chess_v_1 {
 
@@ -176,8 +135,7 @@ abstract class Color {
 }
 
 enum Status {
-    SUCCESS,
-    FAIL
+    SUCCESS, FAIL
 }
 
 class Location {
