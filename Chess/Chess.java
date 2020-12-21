5a9e01cdd06f6e69eaeaae3ac72c197516f8c3c0
akthecoders <akshay@akthecoder.com>
2020-12-21 16:42:38 +0530

    Update Chess Refactor

--- Chess/Chess.java
+++ Chess/Chess.java
@@ -4,20 +4,20 @@
 // import java.util.List;
 
 // class Board {
-//     int rows;
-//     int cols;
-//     List<Piece> pieces = new ArrayList<>();
+// int rows;
+// int cols;
+// List<Piece> pieces = new ArrayList<>();
 // }
 
 // class Player {
-//     String name;
+// String name;
 
-//     public void move(Board board, Piece piece, Location dest) {}
+// public void move(Board board, Piece piece, Location dest) {}
 // }
 
 // interface Piece {
-//     Location location = new Location();
-//     Color color = Color.WHITE;
+// Location location = new Location();
+// Color color = Color.WHITE;
 // }
 
 // class Pawn implements Piece {
@@ -25,46 +25,45 @@
 // }
 
 // enum Color {
-//     WHITE, BLACK
+// WHITE, BLACK
 // }
 
 // class Location {
-//     int row = 0;
-//     int col = 0;
+// int row = 0;
+// int col = 0;
 // }
 
-
 // public class Chess {
-//     Board board;
-//     Player player1;
-//     Player player2;
-//     Player currentPlayer;
+// Board board;
+// Player player1;
+// Player player2;
+// Player currentPlayer;
 
-//     Chess() {
-//         board = new Board();
-//         player1 = new Player();
-//         player2 = new Player();
-//     }
+// Chess() {
+// board = new Board();
+// player1 = new Player();
+// player2 = new Player();
+// }
 
-//     public void play() {
-//         Piece piece = new Pawn();
-//         Location dest = new Location();
-//         currentPlayer.move(board, piece, dest);
-//         if(isWinner(currentPlayer)) {
-//             System.out.println("Player has won " + currentPlayer.name);
-//             return;
-//         }
-//         nextPlayer();
-//     }
+// public void play() {
+// Piece piece = new Pawn();
+// Location dest = new Location();
+// currentPlayer.move(board, piece, dest);
+// if(isWinner(currentPlayer)) {
+// System.out.println("Player has won " + currentPlayer.name);
+// return;
+// }
+// nextPlayer();
+// }
 
-//     public void nextPlayer() {
-//         if(currentPlayer.equals(player1)) {
-//             currentPlayer = player2;
-//         }
-//         else {
-//             currentPlayer = player1;
-//         }
-//     }
+// public void nextPlayer() {
+// if(currentPlayer.equals(player1)) {
+// currentPlayer = player2;
+// }
+// else {
+// currentPlayer = player1;
+// }
+// }
 
-//     public boolean isWinner(Player currentPlayer) {return true;}
+// public boolean isWinner(Player currentPlayer) {return true;}
 // }
